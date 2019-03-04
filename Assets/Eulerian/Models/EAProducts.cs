d3ea648894075355ba2fb767cafbc89bcff3472e
François Rouault <francois.rouault@cocoricostudio.com>
2019-03-04 15:08:19 +0100

    EAProducts

---
+++ Assets/Eulerian/Models/EAProducts.cs
@@ -0,0 +1,19 @@
+﻿namespace eulerian
+{
+    public class EAProducts : EAProperties
+    {
+        private static readonly string KEY_PRODUCTS = "products";
+
+        private readonly JSONArray products = new JSONArray();
+
+        public EAProducts(string path) : base(path)
+        {
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product.json);
+            json.Add(KEY_PRODUCTS, products);
+        }
+    }
+}
