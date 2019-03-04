77a116b3b796d3568c61687ea789778ed51f2b8e
François Rouault <francois.rouault@cocoricostudio.com>
2019-03-04 14:51:36 +0100

    EAEstimate

---
+++ Assets/Eulerian/Models/EAEstimate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace eulerian
+{
+    public class EAEstimate : EAProperties
+    {
+        internal static readonly string KEY_IS_ESTIMATE = "estimate";
+        private static readonly string KEY_REF = "ref";
+        private static readonly string KEY_AMOUNT = "amount";
+        private static readonly string KEY_CURRENCY = "currency";
+        private static readonly string KEY_TYPE = "type";
+        private static readonly string KEY_PRODUCTS = "products";
+        private static readonly string KEY_PRODUCT_AMOUNT = "amount";
+        private static readonly string KEY_QUANTITY = "quantity";
+
+        internal readonly JSONArray products = new JSONArray();
+
+        public EAEstimate(string path, string reference) : base(path)
+        {
+            json[KEY_IS_ESTIMATE] = "1";
+            json[KEY_REF] = reference;
+        }
+
+        public void SetAmount(double amount) => json[KEY_AMOUNT] = amount.ToString();
+        public void SetType(string type) => json[KEY_TYPE] = type;
+
+        /// <summary>
+        /// Sets the currency.
+        /// </summary>
+        /// <param name="currency">Currency. Must be ISO 4217. Exemple: USD, GBP, EUR...</param>
+        public void SetCurrency(string currency) => json[KEY_CURRENCY] = currency;
+
+        public void AddProduct(Product product, double amount, int quantity)
+        {
+            product.json[KEY_PRODUCT_AMOUNT] = amount;
+            product.json[KEY_QUANTITY] = quantity;
+            products.Add(product.json);
+            json[KEY_PRODUCTS] = products;
+        }
+
+    }
+}
--- Assets/SphereScript.cs
+++ Assets/SphereScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using eulerian;
 
 public class SphereScript : MonoBehaviour
 {
@@ -13,7 +14,14 @@ public class SphereScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        EAEstimate prop = new EAEstimate("sphere/enter", "ref");
+        prop.SetCurrency("GBP");
+
+        Product product = new Product("Ref-nut");
+        product.SetName("Nut");
+        prop.AddProduct(product, 2, 4);
+
+        Eulerian.Track(prop);
     }
 
     // Update is called once per frame
